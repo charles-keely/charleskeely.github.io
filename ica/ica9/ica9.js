291039225916d2cc18d589384fed494bc178e0b0
Charles Keely <chke4997@colorado.edu>
2023-05-30 14:28:20 -0600

    yeet

---
+++ ica/ica9/ica9.js
@@ -0,0 +1,25 @@
+
+const button = document.querySelector('#js-new-quote');
+
+const btnClick = button.addEventListener('click', newTrivia);
+
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+
+async function newTrivia(){
+    try{
+        const response = await fetch(endpoint);
+    
+        if(!response.ok){
+            throw Error(response.statusText);
+        }
+
+        const jsonData = await response.json();
+        const quoteText = jsonData['question'];
+        const quoteArea = document.querySelector('#js-quote-text');
+        quoteArea.textContent = quoteText;
+    }
+    catch(err) {
+        console.log(err) 
+        alert('failed');
+    }
+}
